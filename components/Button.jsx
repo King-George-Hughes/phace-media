0835d5c7bac8f094d883518017a1205f60d340a6
unknown <hughesgeorgeking@gmail.com>
2023-08-04 03:08:15 +0000

    changes

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ text }) => {
   return (
-    <button className="inline-block text-gray-900 text-sm md:text-xl bg-white mt-6 md:mt-10 rounded-full px-3 py-1 md:px-8 md:py-2 font-bold">
+    <button className="inline-block text-gray-900 text-sm md:text-xl bg-white mt-6 md:mt-10 rounded-full px-3 py-1 md:px-10 md:py-4 font-bold">
       {text}
     </button>
   );
