2f7e71afb4760fc8353a253b2c554b13c107e890
unknown <hughesgeorgeking@gmail.com>
2023-08-05 16:38:35 +0000

    changes

--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
       className="w-full max-w-[1500px] overflow-hidden"
     >
       {/* Comming Up */}
-      <section className="px-5 md:px-8 text-center mt-5 md:mt-16">
+      <section className="px-5 md:px-8 text-center mt-10 md:mt-16">
         <h1
           variants={slideIn("left", "tween", 0.2, 1)}
           className="text-md text-center my-3 md:text-5xl md:mb-5 uppercase font-bold tracking-widest"
