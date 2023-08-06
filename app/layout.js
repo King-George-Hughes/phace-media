15e8beabbc7d3655e0b8d3e6660d9acd6a9c8816
unknown <hughesgeorgeking@gmail.com>
2023-08-06 16:12:09 +0000

    changes

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Phace Media",
-  description: "A production site for my studio.",
+  description:
+    "Phace Media Studios is a Unique Media House, that stems for the single objective of changing the Narrative, telling African Stories the way they should be told and bridging the media gab between Africa and the outside world.",
 };
 
 export default function RootLayout({ children }) {
@@ -18,7 +19,8 @@ export default function RootLayout({ children }) {
           <div className="gradient-white" />
           <NavBar />
           <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] bg-[#D9D9D9] -right-[160px] -top-[100px] lg:-right-[200px] lg:-top-[120px] z-30 rounded-full" />
-          <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] -left-[160px] -top-[100px] lg:-left-[220px] lg:-top-[170px] -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-white blur-[90px]" />
+          <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] -left-[160px] -top-[100px] lg:-left-[220px] lg:-top-[170px] -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-yellow-900 to-yellow-700 blur-[90px]" />
+          {/* <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] -left-[160px] -top-[100px] lg:-left-[220px] lg:-top-[170px] -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-white blur-[90px]" /> */}
           {children}
           <Footer />
         </main>
