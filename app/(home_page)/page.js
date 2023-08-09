94e545228d1290f3f1d933de8e437efd67a91bc3
unknown <hughesgeorgeking@gmail.com>
2023-08-09 14:25:10 +0000

    changes

--- app/(home_page)/page.js
+++ app/(home_page)/page.js
@@ -1,7 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { Headings, Button, SliderRange } from "@/components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import {
   staggerContainer,
   slideIn,
@@ -13,6 +14,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 
 export default function Home() {
+  const [showSynopsis, setShowSynopsis] = useState(false);
+
   return (
     <motion.main
       variants={staggerContainer}
@@ -22,7 +25,7 @@ export default function Home() {
       className="w-full max-w-[1500px] overflow-hidden"
     >
       {/* Comming Up */}
-      <section className="px-5 md:px-8 text-center mt-10 md:mt-16">
+      <section className="px-5 md:px-8 text-center mt-10 md:mt-16 relative">
         <h1
           variants={slideIn("left", "tween", 0.2, 1)}
           className="text-md text-center my-3 md:text-5xl md:mb-5 uppercase font-bold tracking-widest"
@@ -30,11 +33,52 @@ export default function Home() {
         >
           Coming Up
         </h1>
-        <img
-          src="https://res.cloudinary.com/dg4k5afvg/image/upload/v1691582981/Phace_Media_Banner_2_4ce8406e7f.png"
-          alt="phaace"
-          className="w-full"
-        />
+        <div className="w-full relative">
+          <img
+            src="https://res.cloudinary.com/dg4k5afvg/image/upload/v1691582981/Phace_Media_Banner_2_4ce8406e7f.png"
+            alt="phaace"
+            className="w-full"
+            onClick={() => setShowSynopsis((prevState) => !prevState)}
+            onMouseOver={() => setShowSynopsis(true)}
+            onMouseOut={() => setShowSynopsis(false)}
+          />
+
+          <AnimatePresence>
+            {showSynopsis && (
+              <>
+                <motion.div
+                  initial={{ opacity: 0, y: "100%" }}
+                  animate={{ opacity: 1, y: "-100%" }}
+                  exit={{ opacity: 0, y: "100%" }}
+                  transition={{ duration: 0.2 }}
+                  className="w-full bg-gradient-to-tr from-[rgba(255,255,255,0.2)] to-[rgba(255,255,255,0.3)] rounded-2xl flex flex-col lg:flex-row items-items justify-between p-2 lg:p-10 gap-5 md:gap-10 backdrop-blur-md absolute text-black pointer-events-none"
+                  onMouseOver={(e) => e.stopPropagation()}
+                >
+                  <span className="w-full lg:w-2/3 flex flex-col items-start gap-1 md:gap-2 text-start">
+                    <h5 className="font-bold text-md md:text-2xl text-gray-900">
+                      Synopsis
+                    </h5>
+                    <p className="text-sm md:text-lg font-normal">
+                      Dorm, Abeiku and Snypa are three unemployed graduates
+                      struggling to the teeth to make ends meet. Their destinies
+                      take a different turn as they host 3 daughters of Eve for
+                      a weekend.
+                    </p>
+                  </span>
+                  <span className="w-full lg:w-1/3 flex flex-col items-start gap-1 md:gap-2 text-start">
+                    <h5 className="font-bold text-md md:text-2xl text-gray-900">
+                      Genre
+                    </h5>
+                    <p className="text-sm md:text-lg font-normal">
+                      Comedy, Musical, Political Satire
+                    </p>
+                  </span>
+                  <span></span>
+                </motion.div>
+              </>
+            )}
+          </AnimatePresence>
+        </div>
 
         <Button text={"Buy Ticket"} />
       </section>
--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,7 @@ export default function RootLayout({ children }) {
           <div className="gradient" />
           <div className="gradient-white" />
           <NavBar />
-          <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] bg-[#D9D9D9] -right-[160px] -top-[100px] lg:-right-[200px] lg:-top-[120px] z-30 rounded-full" />
-          {/* <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] -left-[160px] -top-[100px] lg:-left-[220px] lg:-top-[170px] -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-yellow-900 to-yellow-700 blur-[90px]" /> */}
+          <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] bg-[rgba(255,255,255,0.8)] -right-[160px] -top-[100px] lg:-right-[200px] lg:-top-[120px] z-30 rounded-full" />
           <div className="absolute w-[250px] h-[250px] lg:w-[450px] lg:h-[450px] -left-[160px] -top-[100px] lg:-left-[220px] lg:-top-[170px] -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white to-white blur-[90px]" />
           {children}
           <Footer />
